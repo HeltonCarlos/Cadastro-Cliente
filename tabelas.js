58fda569b62faf7610ab771b1805c23edb83d75e
HeltonCarlos <heltonbritopa@gmail.com>
2021-03-30 22:19:10 -0300

    javascript README
    Javacript, aplicação

--- tabelas.js
+++ tabelas.js
@@ -1,4 +1,16 @@
 document.querySelector('#enviar').addEventListener('click',e=>{
-   alert('hey')
+   var nome =document.querySelector('#nome').value
+   var telefone =document.querySelector('#telefone').value
+   var cep =document.querySelector('#cep').value
+   var bairro =document.querySelector('#bairro').value
+   var localidade =document.querySelector('#localidade').value
 
-    })
+   if (!nome || !telefone || !cep || !bairro || !localidade){
+    alert('*Preencha o campo!')}
+
+   var cliente = {nome,telefone, cep, bairro, localidade}
+
+   var cli=localStorage.cli ? JSON.parse(localStorage).[]
+   cli.push(cliente);
+   localStorage.cli=JSON.stringify(cli);
+ })
